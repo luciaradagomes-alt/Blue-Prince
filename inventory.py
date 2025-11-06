1ba4c03646c23396b41f2e41043297c6d17c97b8
Lucia Radanovic Gomes <luciaradagomes@gmail.com>
2025-11-06 21:16:03 +0100

    modification des classes Objet et Inventory pour pouvoir ramasser des objets, ajout de Docstrings

--- inventory.py
+++ inventory.py
@@ -1,13 +1,31 @@
-class Objet:
-    def __init__(self,name,food,steps,locker,chest,price):
-        self.name = name
-        self.food = food
-        self.steps = steps
-        self.locker = locker
-        self.chest = chest
-        self.price = price
+from objet import Objet
 
 class Inventory:
+    """ Classe qui contient les objets dans l'inventaire du joueur.
+
+    Attributes:
+    ---------------
+    - steps : int
+        Le nombre de pas restants au joueur
+    - coins : int
+        Le nombre de pièces d'or que possède le joueur, utilisées pour acheter des produits 
+    - gems : int
+        Le nombre de gemmes que possède le joueur, utilisées pour choisir des salles lors du tirage au sort
+    - keys : int
+        Le nombre de clés que possède le joueur, utilisées pour ouvrir des pièces, des coffres ou des casiers
+    - dice : int
+        Le nombre de dés que possède le joueur, utilisés pour tirer à nouveau les pièces proposées
+    - shovel : bool
+        Indique si le joueur possède une pelle, utilisé pour creuser à certains endroits
+    - hammer : bool
+        Indique si le joueur possède un marteau, utilisé pour briser le cadenas des coffres
+    - lockpit_kit : bool
+        Indique si le joueur possède un kit de crochetage, utilisé pour ouvrir certaines portes sans dépenser de clé
+    - rabbit_foot : bool
+        Indique si le joueur possède un pied de lapin, qui augmente la chance de trouver des objets dans le manoir
+    - metal_detector : bool
+        Indique si le joueur possède un détecteur de métal, qui augmente la chance de trouver des clés ou des pièces d'or dans le manoir
+    """
     
     def __init__(self):
         self.steps = 70
@@ -22,6 +40,8 @@ class Inventory:
         self.metal_detector = False
 
     def show_inventory(self):
+        """ Permet d'afficher l'inventaire du joueur
+        """
     # une fonction pour afficher l'inventaire sur l'interface graphique, à faire avec pygame
     # nécessitera d'images pour les coins, steps, dice, keys
     # pour l'instant je vais juste print les valeurs sur la console
@@ -33,6 +53,7 @@ class Inventory:
         print(f"Gemmes : {self.gems}")
         print(f"Clés : {self.keys}")
         print(f"Dés : {self.dice}")
+        print()
         if self.shovel:
             print("- Pelle")
         if self.hammer:
@@ -42,41 +63,112 @@ class Inventory:
         if self.rabbit_foot:
             print("- Patte de lapin")
         if self.metal_detector:
-            print("- Décteteur de métaux")
+            print("- Détecteur de métaux")
+        print(f"----------------------------------------------------------------------")
 
     def move(self):
         self.steps -= 1
     
     def pick_up(self,object,nb=1):
+        """ Définit les actions réalisées lorsqu'on ramasse un objet dans le manoir
+
+        Parameters:
+        ---------------
+        - object : Objet
+            L'objet qui est recueilli
+        - nb : int (optional)
+            Le nombre d'objets (1 par défaut)
+
+        Raises:
+        ---------------
+            TypeError: Si l'argument n'est pas un objet
+        """
         if type(object) != Objet:
             raise TypeError("Ce n'est pas un objet")
         
+        print(f"-> Trouvé: {nb} {object}", end='')
+        if nb != 1:
+            print("s")
+        else: print()
+
         if object.food:
             self.steps += nb*object.steps
 
-        if object.name == 'coin':
-            self.coins += nb
+        if object.stackable:
+            if object.name == 'pièce':
+                self.coins += nb
 
-        if object.name == 'gem':
-            self.gems += nb
+            if object.name == 'gemme':
+                self.gems += nb
 
-        if object.name == 'key':
-            self.keys += nb
+            if object.name == 'clé':
+                self.keys += nb
+            
+            if object.name == 'dé':
+                self.dice += nb
 
-        if object.locker:
+        if object.locker or object.chest:
+            print(f"Ceci est un {object.name}.", end =' ')
             if self.keys > 0:
-                # demander à l'utilisateur s'il veut ouvrir le locker
-                reponse = True # définir en fonction de la réponse de l'utilsateur
+                reponse = ''
+                while reponse != 'o' and reponse != 'n':
+                    print("Voulez-vous l'ouvrir ?", end=" ")
+                    if object.locker or not self.hammer:
+                        print("Cela coûtera 1 clé.")
+                    else:
+                        print()
+                    reponse = input("Tapez 'o' pour oui et 'n' pour non : ")
+                reponse = (reponse == 'o') 
                 if reponse:
-                    self.keys -= 1
-                    # ajouter les objets qu'on aura trouvés
+                    if object.locker or not self.hammer:
+                        self.keys -= 1
+                    print("Ouvert")
+                    vide = True
+                    for obj in object.objects.items():
+                        if obj[1] > 0:
+                            vide = False
+                            self.pick_up(Objet(obj[0]),obj[1])
+                    if vide:
+                        print("... mais c'était vide :(")
+            else:
+                print("Vous n'avez pas de clés pour l'ouvrir.")
                 
-        if object.chest:
-            if self.keys > 0 or self.hammer:
-                # demander à l'utilisateur s'il veut ouvrir le coffre
-                # ajouter les objets qu'on aura trouvés
-                if not self.hammer:
-                    self.keys -= 1
+        if object.dig_spot:
+            print("Ceci est un endroit creusable.", end=" ")
+            if self.shovel:
+                print("Voulez-vous le creuser ?")
+                reponse = ''
+                while reponse != 'o' and reponse != 'n':
+                    reponse = input("Tapez 'o' pour oui et 'n' pour non : ")
+                reponse = (reponse == 'o')
+                if reponse:
+                    print("Creusé")
+                    vide = True
+                    for obj in object.objects.items():
+                        if obj[1] > 0:
+                            vide = False
+                            self.pick_up(Objet(obj[0]),obj[1])
+                    if vide:
+                        print("... mais c'était vide :(")
+            else:
+                print("Vous n'avez pas de pelle pour le creuser.")
+
+        # Objets spéciaux:
+
+        if object.name == "pelle":
+            self.shovel = True
+
+        if object.name == "patte de lapin":
+            self.rabbit_foot = True
+
+        if object.name == "marteau":
+            self.hammer = True
+
+        if object.name == "kit de crochetage":
+            self.lockpick_kit = True
+        
+        if object.name == "détecteur de métal":
+            self.metal_detector = True
 
     def buy(self,object):
         if type(object) != Objet:
@@ -87,35 +179,65 @@ class Inventory:
         
         self.coins -= object.price
         # ajouter les objets qu'on aura trouvés
-        if object.name == 'shovel':
+        if object.name == 'pelle':
             self.shovel = True
 
         if object.food:
             self.steps += object.steps
+    
+    def open_door(self,niveau):
+        """ Permet au joueur d'ouvrir une porte
+
+        Parameters:
+        ---------------
+        - niveau : int
+            Indique combien de clés le joueur devra dépenser pour ouvrir cette porte
+        """
+        print(f"La porte est vérouillée à clé. Voulez-vous l'ouvrir? Cela coûtera {niveau} clé", end='')
+        if niveau != 1:
+            print("s.")
+        print(".")
+        while reponse != 'o' and reponse != 'n':
+            reponse = input("Tapez 'o' pour oui et 'n' pour non : ")
+        reponse = (reponse == 'o')
+        if reponse:
+            self.keys -= niveau
         
-
-# les objets: chaînes de caractère avec un compteur, à tester pour chaque action? ou autre fonctionnement dans la classe? 
-
-
 #tests
-pomme = Objet('pomme',True,2,False,False,5)
+#pomme = Objet('pomme')
 
 inventory = Inventory()
 
-inventory.pick_up(pomme)
+#inventory.show_inventory()
+
+#inventory.pick_up(pomme)
+
+#inventory.show_inventory()
+
+cle = Objet("clé")
+
+inventory.pick_up(cle,4)
+
+casier = Objet("casier")
+
+inventory.pick_up(casier)
 
 inventory.show_inventory()
 
-for i in range(10):
-    inventory.move()
+coffre = Objet("coffre")
+inventory.pick_up(Objet("marteau"))
+inventory.pick_up(coffre)
 
 inventory.show_inventory()
 
-inventory.pick_up(Objet('coin',False,0,False,False,0),5)
+creuse = Objet("endroit creusable")
+
+inventory.pick_up(creuse)
 
 inventory.show_inventory()
 
-inventory.buy(pomme)
+pelle = Objet("pelle")
+inventory.pick_up(pelle)
+inventory.pick_up(creuse)
 
 inventory.show_inventory()
-
---
+++ objet.py
@@ -0,0 +1,178 @@
+import random
+
+class Objet:
+    """ Classe qui définit les objets contenus dans le jeu
+
+    Attributes:
+    ---------------
+    - possible : list[str] <<class attribute>>
+        Liste de tous les objets dans notre jeu
+    - name : str
+        Nom de l'objet, parmi les possibilités dans la Liste possible
+    - food : bool
+        Détermine si l'objet est de la nourriture
+    - locker : bool
+        Détermine si l'objet est un casier
+    - chest : bool
+        Détermine si l'objet est un coffre
+    - dig_spot : bool
+        Détermine si l'endroit est un endroit creusable
+    - stackable : bool
+        Détermine si l'objet est stackable (pièces d'or, gemmes, clés et dés)
+    """
+
+    possible = ["pomme","banane","gâteau","repas","sandwich", "repas", 
+                "pièce", "gemme", "clé", "dé",
+                "coffre", "casier", "endroit creusable",
+                "pelle", "marteau", "patte de lapin", "kit de crochetage", "détecteur de métal", "levier cassé"]
+
+    def __init__(self, name):
+        self.name = name
+        self.food = False
+        self.locker = False
+        self.chest = False
+        self.dig_spot = False
+        self.stackable = False
+
+        self.steps = 0
+        self.objects = None
+
+        self.is_food()
+        self.is_stackable()
+        self.is_locker()
+        self.is_chest()
+        self.is_dig_spot()
+
+    @property
+    def name(self):
+        return self.__name
+    
+    @name.setter
+    def name(self,name):
+        if name not in Objet.possible:
+            raise ValueError("Ce n'est pas un objet")
+        self.__name = name
+
+    def is_stackable(self):
+        stackable_objects = ["pièce", "gemme", "clé", "dé"]
+        if self.name in stackable_objects:
+            self.stackable = True
+
+    def is_food(self):
+        food = {"pomme" : 2, "banane" : 3, "gâteau" : 10, "sandwich" : 15, "repas" : 25}
+        if self.name in food:
+            self.food = True
+            self.steps = food[self.name]
+
+    def is_locker(self):
+        proba = random.randint(1, 100)
+
+        if self.name == "casier":
+            self.locker = True
+            self.required_tool = "clé"   # L'outil nécessaire pour ouvrir le locker
+            if proba <= 50:             
+                self.objects = {}
+
+            elif proba <= 80 :
+                self.objects = {"gemme" : 0, "pièce" :  0}  
+                for i in range(1, 4):
+                    valeur = random.randint(1,len(self.objects))
+                    if valeur == 1:
+                        self.objects["gemme"] += 1
+                    else:
+                        self.objects["pièce"] += random.randint(1, 5)  
+                
+            else :
+                self.objects = {"pomme" : 0, "clé" : 0, "gemme": 0, "pièce" : 0}
+
+                for i in range(1, 3):
+                    valeur = random.randint(1,len(self.objects))
+
+                    if valeur == 1:
+                        self.objects["pomme"] += 1
+                    elif valeur == 2:
+                        self.objects["clé"] += 1
+                    elif valeur == 3:
+                        self.objects["gemme"] += 1
+                    else:
+                        self.objects["pièce"] += random.randint(1, 5)   
+            
+    def is_chest(self):
+        proba = random.randint(1, 100)
+
+        if self.name == "coffre":
+            self.chest = True
+            self.required_tool = "clé"   # L'outil nécessaire pour ouvrir le coffre
+            if proba <= 50:             
+                self.objects = {}
+
+            elif proba <= 80 :
+                self.objects = {"gemme" : 0, "pièce" :  0}  
+                for i in range(1, 4):
+                    valeur = random.randint(1,len(self.objects))
+                    if valeur == 1:
+                        self.objects["gemme"] += 1
+                    else:
+                        self.objects["pièce"] += random.randint(1, 5)  
+                
+            else :
+                self.objects = {"pomme" : 0, "clé" : 0, "gemme": 0, "pièce" : 0}
+
+                for i in range(1, 3):
+                    valeur = random.randint(1,len(self.objects))
+
+                    if valeur == 1:
+                        self.objects["pomme"] += 1
+                    elif valeur == 2:
+                        self.objects["clé"] += 1
+                    elif valeur == 3:
+                        self.objects["gemme"] += 1
+                    else:
+                        self.objects["pièce"] += random.randint(1, 5)   
+                
+            
+    def is_dig_spot(self):
+        proba = random.randint(1, 100)
+
+        if self.name == "dig_spot":
+            self.dig_spot = True
+            if proba <=30:
+                self.objects = {}
+                
+            elif proba <=80:
+                self.objects = {"clé" : 0, "pièce" : 0, "levier cassé" : 0}
+                for i in range(1, 3):
+                    valeur = random.randint(1,len(self.objects))
+
+                    if valeur == 1:
+                        self.objects["clé"] += 1
+                    elif valeur == 2:
+                        self.objects["pièce"] += random.randint(1, 4)
+                    else:
+                        self.objects["levier cassé"] += 1  
+                
+            else:
+                self.objects = {"dés" : 0, "gemme" : 0}
+                for i in range(1, 3):
+                    valeur = random.randint(1,len(self.objects))
+                    if valeur == 1:
+                        self.objects["dés"] += random.randint(1, 3) 
+                    else:
+                        self.objects["gemme"] += random.randint(1, 3)  
+    
+    def __str__(self):
+        return self.name
+        
+#%%
+# tests
+
+pomme = Objet("pomme")
+#print(f"{pomme.name} : nourriture ? {pomme.food}, stackable ? {pomme.stackable}, nombre de pas : {pomme.steps}")
+
+coffre = Objet("coffre")
+#print(f"{coffre.name} : nourriture ? {coffre.food}, stackable ? {coffre.stackable}, outil nécessaire pour l'ouvrir : {coffre.required_tool}, objets dans le {coffre.name} : {coffre.objects}")
+
+casier = Objet("casier")
+#print(f"{casier.name} : nourriture ? {casier.food}, stackable ? {casier.stackable}, outil nécessaire pour l'ouvrir : {casier.required_tool}, objets dans le {casier.name} : {casier.objects}")
+
+# %%
